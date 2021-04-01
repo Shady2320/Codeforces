637b70c559a8abc184b5ef8679a7c0438e5107cf
Shady2320 <71116840+Shady2320@users.noreply.github.com>
2021-04-01 22:25:03 +0530

    Add files via upload

---
+++ Isitrated.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	string s;
+	getline(cin,s);
+	while(s.length()>0)
+	{
+		cout<<"NO"<<endl;
+		//cout<<flush;
+		getline(cin,s);
+	}
+	return 0;
+}
